d5f9fc22d235f1d46653f94b9750d32e95adfa0f
Prabhakar Undurthi <undurthi_prabhakar@aol.com>
2014-12-24 14:10:53 +0530

    Added tests for users profile

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "a user should enter their first name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+  end
+
+  test "user should enter last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+  end
+
+  test "user should enter profile name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
+
+  test "user should enter uniq profile name" do
+    user = User.new
+    user.profile_name = "PrabhakarUndurthi"
+    user.first_name   = "Prabhakar"
+    user.last_name    = "Undurthi"
+    user.email        = "prabhakar@railsmancer.com"
+    user.password     = "password"
+    user.password_confirmation = "password"
+    assert !user.save
+
+    puts user.errors.inspect
+
+    assert !user.errors[:profile_name].empty?
+  end
+
+  test "profile name should be formatted correctly" do
+    user = User.new
+    user.profile_name = "Profile Name with spaces"
+    assert !user.save
+    assert user.errors[:profile_name].include?("Must be formatted correctly")
+  end
 end
